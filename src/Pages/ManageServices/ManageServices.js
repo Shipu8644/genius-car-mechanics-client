157950e0e08828e692110745432e3fb7b93e8bd4
Shipu8644 <shipu.monjurur@gmail.com>
2021-10-27 19:53:37 +0600

    deploy done successfully

--- src/Pages/ManageServices/ManageServices.js
+++ src/Pages/ManageServices/ManageServices.js
@@ -3,13 +3,13 @@ import React, { useEffect, useState } from 'react';
 const ManageServices = () => {
     const [services, setServices] = useState([]);
     useEffect(() => {
-        fetch('http://localhost:5000/services')
+        fetch('https://serene-hollows-08213.herokuapp.com/services')
             .then(res => res.json())
             .then(data => setServices(data))
     }, [])
     const deleteServiceHandle = id => {
         console.log(id);
-        const url = `http://localhost:5000/services/${id}`;
+        const url = `https://serene-hollows-08213.herokuapp.com/services/${id}`;
         fetch(url, {
             method: "DELETE"
         })
